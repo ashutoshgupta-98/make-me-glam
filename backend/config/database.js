4f245c88da682846822f6854c20822a22ad60c62
ashutoshgupta-98 <ag23gupta@gmail.com>
2026-09-11 23:37:01 +0530

    First commit - make me glam project

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,35 @@
+// const { Pool } = require("pg");
+// require("dotenv").config();
+
+// const pool = new Pool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     database: process.env.DB_NAME,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     max: 10,
+//     idleTimeoutMillis: 30000,
+//     connectionTimeoutMillis: 2000
+// });
+
+// pool.on("connect", () => {
+//     console.log("PostgreSQL database connected");
+// });
+
+// pool.on("error", (err) => {
+//     console.error("Unexpected PostgreSQL error:", err);
+// });
+
+// module.exports = pool;
+
 const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // Neon ya external cloud databases ke liye zaroori hai
+    },
     max: 10,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,7 +3,10 @@
 // ==========================================
 
 // const API_BASE_URL = "http://localhost:5000/api";
-const API_BASE_URL = 'https://make-me-glam.onrender.com/api';
+// const API_BASE_URL = 'https://make-me-glam.onrender.com/api';
+const API_BASE_URL = window.location.hostname === "localhost" 
+    ? "http://localhost:5000/api" 
+    : "https://make-me-glam.onrender.com/api";
 
 
 // ==========================================
